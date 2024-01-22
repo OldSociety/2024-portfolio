02837717f5e38a496d47076731f19c71a426a5d9
austin.lance@smoothstack.com <77307519+OldSociety@users.noreply.github.com>
2024-01-22 10:59:29 -0800

    header image

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 import React from 'react'
 
@@ -5,17 +7,35 @@ export default function Intro() {
   return (
     <section>
       <div className="flex items-center justify-center">
-        <div>
-          <Image
-            src="/headshot.jpg"
-            alt="austin headshot"
-            width="192"
-            height="192"
-            quality="95"
-            priority={true}
-            className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-          />
-          <span className="text-4xl">👋🏾</span>
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: 'tween', duration: 0.2 }}
+          >
+            <Image
+              src="/headshot.jpg"
+              alt="austin headshot"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0  text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋🏾
+          </motion.span>
         </div>
       </div>
     </section>
