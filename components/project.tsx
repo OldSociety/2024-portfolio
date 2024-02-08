5155fc4326ffd06a2ffef13ffa2a69189ca93755
austin.lance@smoothstack.com <77307519+OldSociety@users.noreply.github.com>
2024-02-07 16:56:57 -0800

    rounded corners, flip image

--- components/project.tsx
+++ components/project.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { projectsData } from '@/lib/data'
-import { motion, useScroll } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
 import { useRef } from 'react'
 import Image from 'next/image'
 
@@ -18,14 +18,16 @@ export default function Project({
     target: ref,
     offset: ['0 1', '1.33 1'],
   })
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1])
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1])
 
   return (
     <motion.div
-      className="mb-3 sm:mb-8 last:mb-0 "
+      className="group mb-3 sm:mb-8 last:mb-0 "
       ref={ref}
-      style={{ scale: scrollYProgress, opacity: scrollYProgress }}
+      style={{ scale: scaleProgress, opacity: opacityProgress }}
     >
-      <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] even:pl-8 hover:bg-gray-200 transition">
+      <section className=" bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] group-even:pl-8 hover:bg-gray-200 transition">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[26rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
